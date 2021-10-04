fb89f7604ea437b3482e2ccca062b25f09a6021f
Tung Lam <trant38@unlv.nevada.edu>
2021-10-04 15:51:53 +0700

    Constructor

--- CAT.java
+++ CAT.java
@@ -3,10 +3,15 @@ public class CAT {
   public int age;
   public float weight;
 
+  CAT(String catName, int catAge, float catWeight){
+        name = catName;
+        age = catAge;
+        weight = catWeight;
+  } // tạo 1 constructor, giúp ng dùng không phải suốt ngày get and set
   public void setName(String name){
-      this.name = name;
+    this.name = name;
   }
-  public void setAge(int age){
+  public void setAge(int age){ // void means no return needed
     this.age = age;
   }
   public String getName(){
@@ -14,6 +19,9 @@ public class CAT {
   }
   public int getAge(){
       return age;
+  }
+  public float getWeight(){
+    return weight;
   }
   public void speak(){
       System.out.println("hi, my name " + name + " " );
@@ -28,6 +36,6 @@ public class CAT {
   }
   public void eat(){
       System.out.println(name + "eating");
-
+  
   }
 }
--- classObject.java
+++ classObject.java
@@ -3,13 +3,15 @@
 public class classObject {
   public static void main(String[] args) {
    
-    CAT cat = new CAT();
-
-    cat.setName("TOM");
-    cat.setAge(3);
-    cat.speak();
+    CAT cat = new CAT("TOM", 3, 10); // đây là phương thức khởi tạo (constructors), chứa tham số, cùng tên với class, 
+    // mỗi lần khởi tạo 1 class thì constructors sẽ tạo thuộc tính (initialize object' attributes) right away
     
-    cat.getName();
-    cat.getAge();
+    System.out.println(cat.getName());
+    
+    
+    System.out.println(cat.getAge());
+    
+    
+    System.out.println(cat.getWeight());
   }
 }
